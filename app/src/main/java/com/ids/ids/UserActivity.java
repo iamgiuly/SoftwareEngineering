395bf186515866e6546335eec049bdbae3e39f3e
Andrea <arubec95@gmail.com>
2018-03-06 11:35:16 +0100

    .

---
+++ app/src/main/java/com/ids/ids/UserActivity.java
@@ -0,0 +1,42 @@
+package com.ids.ids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //ciao
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void visualizzazioneBottoneInvio(){
+
+    }
+
+    public void visualizzazioneMessaggioRiconnetti(){
+
+
+    }
+
+
+    public void visualizzazioneMappa(){
+
+    }
+
+    public void tapNodiSottoIncendio(){
+
+    }
+
+    public void tapBottoneInvioNodi(){
+
+    }
+
+}
+
+
---
+++ app/src/main/java/com/ids/ids/UserController.java
@@ -0,0 +1,10 @@
+package com.ids.ids;
+
+/**
+ * Created by Andrea on 06/03/2018.
+ */
+
+public class UserController {
+    public void visualizzaActivity(){
+    }
+}
