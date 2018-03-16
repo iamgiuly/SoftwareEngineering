c52ab565eccbfa109f893a9702f7dc728c9a7532
Luca Rossi <rossiluca711@gmail.com>
2018-03-16 21:40:25 +0100

    fds

--- app/src/main/java/com/ids/ids/utils/DebugSettings.java
+++ app/src/main/java/com/ids/ids/utils/DebugSettings.java
@@ -1,7 +1,7 @@
 package com.ids.ids.utils;
 
 public class DebugSettings {
-    public static boolean SEED_DB = false;
-    public static boolean SCAN_BLUETOOTH = false;
+    public static boolean SEED_DB = true;
+    public static boolean SCAN_BLUETOOTH = true;
     public static boolean CHECK_WIFI = false;
 }
